df5b354ff468638d45ce6ebd8d99b2a2ed80b032
Erika Antonella Miño <erikaantonellamino@gmail.com>
2023-04-25 11:50:46 -0300

    se creo el archivo pruebas para probar los filtros

--- pruebas.php
+++ pruebas.php
@@ -6,12 +6,15 @@ $juegos = "SELECT * FROM juegos";
 $resJuegos = $link->query($juegos);
 $generos = "SELECT nombre, id FROM generos";
 $resGeneros = $link->query($generos);
-$plataformas = "SELECT nombre FROM plataformas";
+$plataformas = "SELECT * FROM plataformas";
 $resPlataformas = $link->query($plataformas);
+
+print_r ($_POST);
 
 
 if(isset($_POST['filtrar'])){
-    if((!empty($_POST['juego_a_buscar'])) and (empty($_GET['genero'])) and (empty($_POST['plataforma']))){
+    //FILTRAR POR JUEGO
+    if((!empty($_POST['juego_a_buscar'])) and (empty($_POST['genero'])) and (empty($_POST['plataforma']))){
         $nombre = $_POST['juego_a_buscar'];
         $consulta = "SELECT * FROM juegos WHERE nombre LIKE '%$nombre%'";
         $resConsulta = $link->query($consulta);
@@ -20,7 +23,33 @@ if(isset($_POST['filtrar'])){
                 echo $campo['descripcion'].'<br>';
                 echo $campo['url'].'<br>';
             }
-        }    
+        }   
+    
+    //FILTRAR POR GENERO
+    if((empty($_POST['juego_a_buscar'])) and (!empty($_POST['filtro_genero']) and ($_POST['filtro_genero']!= "seleccionar")) and (empty($_POST['plataforma']))){
+        $genero = $_POST['filtro_genero'];
+        $consulta = "SELECT * FROM juegos WHERE id_genero = $genero";
+        $resConsulta = $link->query($consulta);
+        while($campo = $resConsulta->fetch_assoc()){
+            echo $campo['nombre'].'<br>';
+            echo $campo['descripcion'].'<br>';
+            echo $campo['url'].'<br>';
+            echo $campo['id_genero']; //COMO HAGO PARA QUE ME PONGA EL NOMBRE DEL GENERO
+        }
+    }
+
+    //FILTRAR POR PLATAFORMA
+    if(((!empty($_POST['filtro_plataforma']) and ($_POST['filtro_plataforma']!= "seleccionar")) and (empty($_POST['filtro_genero'])) and (empty($_POST['juego_a_buscar'])))){
+        $plataforma = $_POST['filtro_plataforma'];
+        $consulta = "SELECT * FROM juegos WHERE id_plataforma = $plataforma";
+        $resConsulta = $link->query($consulta);
+        while($campo = $resConsulta->fetch_assoc()){
+            echo $campo['nombre'].'<br>';
+            echo $campo['descripcion'].'<br>';
+            echo $campo['url'].'<br>';
+            echo $campo['id_plataforma'];
+        }
+    }
 }
 
 
@@ -58,24 +87,25 @@ if(isset($_POST['filtrar'])){
 
 
     <!--------formulario para filtrar--------->
-    <form method="POST" action="index2.php"> 
+    <form method="POST" action="pruebas.php"> 
         <h2>Filtrar juego por: </h2>
         <label for="nombre">Nombre del juego: </label>
         <input type="text" name="juego_a_buscar" id="nombre">
 
 
         <label for="genero"> Genero </label>
-            <select name="filtro_genero" id="genero"> <!--id se asocia con el name del select-->
-            <option>Seleccionar</option>
+        <select name="filtro_genero" id="genero"> <!--id se asocia con el name del select-->
+            <option value="seleccionar">Seleccionar</option>
             <?php while ($row = $resGeneros->fetch_assoc()){?>
-                <?php echo "<option value=\"" . $row["id"] . "\">" . $row["nombre"] . "</option>"; ?>
-                <?php } ?>
-            </select>
+            <?php echo "<option value=\"" . $row["id"] . "\">" . $row["nombre"] . "</option>"; ?>
+            <?php } ?>
+        </select>
 
 
         <label for="plataforma"> Plataforma </label>
         <select name="filtro_plataforma" id="plataforma">
-        <?php while ($row = $resPlataformas->fetch_assoc()){?>
+            <option value="seleccionar">Seleccionar</option>
+            <?php while ($row = $resPlataformas->fetch_assoc()){?>
             <?php echo "<option value=\"" . $row["id"] . "\">" . $row["nombre"] . "</option>"; ?>
             <?php } ?>
         </select>
@@ -102,7 +132,13 @@ if(isset($_POST['filtrar'])){
     <!------------- juegos disponibles--------------->
     
     <!-- aca iria la imagen -->
-    <?php while ($row = $resJuegos->fetch_assoc()){ ?>
+
+    <?php $consulta = "SELECT j.*, g.nombre AS nombre_genero, p.nombre as nombre_plataforma FROM juegos j 
+                        INNER JOIN generos g ON g.id = j.id_genero 
+                        INNER JOIN plataformas p on p.id = j.id_plataforma";
+        $resConsulta = $link->query($consulta);
+    while ($row = $resConsulta->fetch_assoc()){
+    ?>
     <div class="lista-juegos">
         <ul>
             <li>
@@ -110,8 +146,8 @@ if(isset($_POST['filtrar'])){
                     <div class="card-juegos-item">
                         <li>NOMBRE: <?php echo $row["nombre"]; ?></li>
                         <li>DESCRIPCION: <?php echo $row["descripcion"]; ?> </li>
-                        <li>GENERO: <?php echo $gen["nombre"]; ?></li>
-                        <li>PLATAFORMA: <?php echo $plat["nombre"];?></li>
+                        <li>GENERO: <?php echo $row["nombre_genero"]; ?></li>
+                        <li>PLATAFORMA: <?php echo $row["nombre_plataforma"];?></li>
                         <li>URL:<?php echo $row["url"]; ?></li>
                     </div>
                 </ul>
@@ -122,11 +158,8 @@ if(isset($_POST['filtrar'])){
 
 
 
-
-
     <!----------- pie de pagina ----------->
     <footer>Miño Erika Antonella - Cotignola Griselda Soledad - 2023</footer>
-    <!-- <script src="validarAltaJuego.js"></script> -->
 </body>
 
 </html>
